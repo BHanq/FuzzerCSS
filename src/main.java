2b0dd9fd67746f4e5b33f17b53becf9fd13070b8
Krikium <krikium@localhost.localdomain>
2020-06-15 13:14:23 +0200

    Found 5th

--- src/main.java
+++ src/main.java
@@ -1,15 +1,9 @@
-
-import hanquin.utils.Utils;
-
-import static java.nio.file.StandardOpenOption.*;
+import static java.lang.Thread.sleep;
 
 import java.io.*;
-import java.nio.file.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Arrays;
-import java.lang.reflect.Array;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.Random;
@@ -17,52 +11,80 @@ import java.util.Random;
 public class main {
     static String outputName = "outputBenja";
     static int outputNumberFile = 1;
+    static int inputNumber = 0;
+    static byte[] magicNb,version, author,WidthHeightHeader,Width,Height,ColTaSizeHeader,ColTaSize,ColTableHeader,ColTable,commentHeader,comment,Pixels,AABytes;
 
     public static void main(String[] args) {
-        //Magic Number
-        // Convert the string to a
-        // byte array.
-        String s = "Hello World! ";
-        byte data[];
-        Path p = Paths.get("./inputBenja.img");
-        List<Integer> array = new LinkedList<Integer>();
-
-
         try {
-            OutputStream os = new FileOutputStream("inputBenja.img");
-            //Magic Number
-            byte[] magicNb = hexStringToByteArray("ABCD");
-
-            byte[] version = hexStringToByteArray("6400");//
-            byte[] author = hexStringToByteArray("0152616D696E00");
-            byte[] WidthHeight = hexStringToByteArray("020200000000000000");
-            byte[] ColTaSize = hexStringToByteArray("0A02000000");
-            byte[] ColTable = hexStringToByteArray("0B00000000FFFFFF00");
-            byte[] comment = randomByteGenerator(1300);//hexStringToByteArray("0C48656C6C6F00");
-            byte[] Pixels = hexStringToByteArray("00010100");
 
 
-            os.write(magicNb);
-            os.write(version);
-            os.write(author);
-            os.write(WidthHeight);
-            os.write(ColTaSize);
-            os.write(ColTable);
-            os.write(comment);
-            os.write(Pixels);
+//             for(short i = Short.MIN_VALUE ; i <Short.MAX_VALUE; i++) {//for(short i = 80 ; i <103; i++) {//
+            for(int i = 0; i <400000; i=i+50000) {//for(short i = 80 ; i <103; i++) {//
+                //for (int j = 0; j < 258; j++) {
+                    inputNumber++;
+                    FileOutputStream os = new FileOutputStream("inputBenja" + inputNumber + ".img");
+                    System.out.println("");
+                    System.out.print(i + " ");
 
 
-            os.close();
-            try {//./converter_linux_x8664
-                String outputRealName = outputName + outputNumberFile + ".img";
-                executeCommandLine("./converter_linux_x8664 inputBenja.img " + outputRealName, false, false, 1);
-            } catch (Exception e) {
-                System.out.println("Error in Exec Command");
-            }
+                    //Magic Number
+                    magicNb = hexStringToByteArray("ABCD");//shortToByte((short) i, true);//
+
+                    version = shortToByte((short) 100, true);//hexStringToByteArray("6400");//
+                    author = hexStringToByteArray("0152616D696E00");//randomByteGenerator(10000);//
+                 WidthHeightHeader = hexStringToByteArray("02");
+                     Width = intToByte(4, i, true);//hexStringToByteArray("02000000");//intToByte(4,2,true);
+                     //Height = intToByte(4, i, true);
+                     ColTaSizeHeader = hexStringToByteArray("0A");
+                    ColTaSize = intToByte(4, 2, true);//hexStringToByteArray("08000000");
+
+                     ColTableHeader = hexStringToByteArray("0B");
+                      ColTable = hexStringToByteArray("FFFFFFFFFFFFFFFF");
+
+
+                      commentHeader = hexStringToByteArray("0C");
+                      comment = hexStringToByteArray("0C48656C6C6F00");
+
+                      Pixels = hexStringToByteArray("00010100");
+                      AABytes = hexStringToByteArray("AA");
+//                    comment = new byte[1800];
+//                Arrays.fill(comment,(byte) 1);
+
+                    os.write(magicNb);
+                    os.write(version);
+                    os.write(author);
+                    os.write(WidthHeightHeader);
+                    os.write(Width);
+                    os.write(Width);
+                    os.write(ColTaSizeHeader);
+                    os.write(ColTaSize);
+                    os.write(ColTableHeader);
+                    os.write(ColTable);
+                    os.write(commentHeader);
+                    os.write(comment);
+                os.write(Pixels);
+//                Pixels = hexStringToByteArray("00");
+//                for(int width= 0 ;width<32000;width++){
+//                    for(int height= 0 ;height<1;height++){
+//
+//                        os.write(Pixels);
+//
+//                    }
+//                    Pixels = hexStringToByteArray("01");
+//                }
+                    os.close();
+
+                    try {//./converter_linux_x8664
+                        String outputRealName = outputName + ".img";
+                        executeCommandLine("./converter_linux_x8664 inputBenja" + inputNumber + ".img " + outputRealName, false, false, 10);
+                    } catch (Exception e) {
+                        System.out.println("Error in Exec Command");
+                    }
+                }
+            //}
         } catch (IOException x) {
             System.err.println(x);
         }
-
     }
 
 
@@ -78,20 +100,21 @@ public class main {
         String line ;
         Boolean error =false;
         while ((line = stdInput.readLine ()) != null) {
-            System.out.println(line);
+            System.out.print(line);
             if(line.equals("*** The program has crashed ***")){
                 error=true;
             }
         }
         Runtime runtime2 = Runtime.getRuntime();
         if (error) { //Program crashed
-            Process process2 = runtime2.exec("mv  inputBenja.img inputFile" + outputNumberFile + ".img");
+            executeCommand("mv  inputBenja" +inputNumber + ".img inputFile" + outputNumberFile + ".img");
             outputNumberFile++;
-            System.out.println("On en a TROUVE 1 !!!!!!!!!!!!!!!");
+            System.out.print("On en a TROUVE 1 !!!!!!!!!!!!!!!");
         } else {
-            runtime2.exec("rm " + outputName + outputNumberFile + ".img");
-            //runtime2.exec("rm  inputBenja.img");
-            System.out.println("rm " + outputName + outputNumberFile + ".img");
+            //System.out.print("Pas de crash");
+            runtime2.exec("rm " + outputName + ".img");
+            //runtime2.exec("rm  inputBenja" +inputNumber +".img");
+            //System.out.println("rm " + outputName + outputNumberFile + ".img");
 
         }
         if (!process.waitFor(timeout, TimeUnit.MINUTES)) {
@@ -100,40 +123,45 @@ public class main {
         }
     }
 
-    private static class Worker extends Thread {
-        private final Process process;
-        private Integer exit;
+    public static void executeCommand(final String commandLine){
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec(commandLine);
+        }catch (Exception ex){
+            System.out.println(ex);
+        }
+        return;
+    }
 
-        private Worker(Process process) {
-            this.process = process;
+public static byte[] intToByte(int size,int transformInt, boolean toLittleEndian){
+    ByteBuffer byteBuffer = ByteBuffer.allocate(size);
+    if (toLittleEndian){
+        byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+    }
+    byteBuffer.putInt(transformInt);
+    byte[] bytes =byteBuffer.array();
+
+//    for (byte b : bytes) {
+//        System.out.format("0x%x ", b);
+//    }
+    return byteBuffer.array();
+}
+
+public static byte[] shortToByte(short transformShort, boolean toLittleEndian){
+        ByteBuffer byteBuffer = ByteBuffer.allocate(2);
+        if (toLittleEndian){
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
         }
 
-        public void run() {
-            try {
-                exit = process.waitFor();
-            } catch (InterruptedException ignore) {
-                return;
-            }
-        }
+        byteBuffer.putShort(transformShort);
+        return byteBuffer.array();
     }
 
     public static byte[] randomByteGenerator(int length) {
-        String TotHex = "";
-        int actualLength = length;
-        Random random = new Random();
-        do {
-            int maxLength;
-            if(actualLength>4){maxLength = 4; } else {maxLength=actualLength;}
-            int val = random.nextInt((int) Math.pow(16, maxLength * 2));
-            String Hex = Integer.toHexString(val);
-            if (Hex.length() % 2 == 1) {
-                Hex = "0" + Hex;
-            }
-            actualLength -= 4; //Int allow max 4 bytes
-            TotHex += Hex;
-        } while (TotHex.length() != length * 2);
-        System.out.println(TotHex);
-        return hexStringToByteArray(TotHex);
+        Random rd = new Random();
+        byte[] arr = new byte[length];
+        rd.nextBytes(arr);
+        return arr;
     }
 
     public static byte[] hexStringToByteArray(String s) {
